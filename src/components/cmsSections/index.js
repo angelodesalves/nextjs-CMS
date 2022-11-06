6111a56cee0871873babdb1f3690ef8644c388b2
angelodesalves <angelodesalves@gmail.com>
2022-11-06 16:10:07 -0300

    Aula 4.1 - Migrando a home para integração completa via CMS

---
+++ src/components/cmsSections/index.js
@@ -0,0 +1,11 @@
+import { Footer } from "../commons/Footer";
+import { Menu } from "../commons/Menu";
+import SeoBlock from "./SeoBlock";
+import PageHomeHeroSectionRecord from "./PageHomeHeroSectionRecord";
+
+export const cmsSections = {
+	CommonSeoBlockRecord: SeoBlock,
+	CommonMenuRecord: Menu,
+	PagehomeHerosectionRecord: PageHomeHeroSectionRecord,
+	CommonFooterRecord: Footer
+};
